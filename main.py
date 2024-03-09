709a62a7c72327f8cc8e140ab02ba1d4a0a000ea
Buck Baskin <mobile.wbaskin@gmail.com>
2024-03-08 16:37:24 -0800

    Explore working up to downloading new records

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from collections import defaultdict
 import json
 import code
 
+
 def interact():
     code.InteractiveConsole(locals=globals()).interact()
 
-print('Hello World')
+
+print("Hello World")
+
 
 class InteractionsCounter(object):
     def __init__(self):
-        self.previous_func = '_init'
+        self.previous_func = "_init"
 
         self.usage = defaultdict(int)
         self.sequences = defaultdict(int)
@@ -19,29 +22,30 @@ class InteractionsCounter(object):
 
     def count(self, func_name):
         self.usage[func_name] += 1
-        self.sequences[f'{func_name}:{self.previous_func}'] += 1
+        self.sequences[f"{func_name}:{self.previous_func}"] += 1
 
         self.previous_func = func_name
 
         self._serialize()
 
     def _serialize(self):
-        string_version = json.dumps({'usage': self.usage, 'sequences': self.sequences})
-        with open('interactions.json', 'w') as f:
+        string_version = json.dumps({"usage": self.usage, "sequences": self.sequences})
+        with open("interactions.json", "w") as f:
             f.write(string_version)
 
     def _deserialize(self):
         try:
-            with open('interactions.json', 'r') as f:
+            with open("interactions.json", "r") as f:
                 py_version = json.load(f)
 
-            self.usage = defaultdict(int, py_version['usage'])
-            self.sequences = defaultdict(int, py_version['sequences'])
+            self.usage = defaultdict(int, py_version["usage"])
+            self.sequences = defaultdict(int, py_version["sequences"])
         except FileNotFoundError:
             pass
 
 
 _interactions = InteractionsCounter()
+
 
 def track_usage(func):
     @functools.wraps(func)
@@ -53,21 +57,43 @@ def track_usage(func):
 
     return wrap
 
-class RenderWrapper(object):
+
+class RenderExplorer(object):
     def __init__(self, name):
         self.name = name
 
     def __call__(self):
-        print('RenderWrapper.__call__')
-        return f'RenderWrapper({self.name})'
+        print("RenderExplorer.__call__")
+        return f"RenderExplorer({self.name})"
 
     def __repr__(self):
-        print('RenderWrapper.__repr__')
-        return f'RenderWrapper({self.name})'
+        print("RenderExplorer.__repr__")
+        return f"RenderExplorer({self.name})"
 
     def __str__(self):
-        print('RenderWrapper.__str__')
-        return f'RenderWrapper({self.name})'
+        print("RenderExplorer.__str__")
+        return f"RenderExplorer({self.name})"
+
+
+class RenderRecord(object):
+    def __init__(self, record):
+        self.record = record
+
+    def __repr__(self):
+        print("RenderExplorer.__repr__")
+        return f"RenderExplorer({self.record})"
+
+    def __str__(self):
+        print("RenderExplorer.__str__")
+
+        def lines():
+            yield self.record.title
+            yield f"Scores: PRNG {self.record['prng_score']} TFIDF {self.record['tfidf_score']} Citation {self.record['citation_score']}"
+            yield ""
+            yield from self.record.abstract.split("\n")
+
+        return "\n".join(lines)
+
 
 class UserInterface(object):
     def __init__(self):
@@ -77,15 +103,15 @@ class UserInterface(object):
 
         self.active_item = None
 
-        self.mark_as_interested = RenderWrapper('mark_as_interested')
-        self.mark_as_read = RenderWrapper('mark_as_read')
-        self.mark_as_liked = RenderWrapper('mark_as_liked')
-        self.mark_as_disliked = RenderWrapper('mark_as_disliked')
+        self.mark_as_interested = RenderExplorer("mark_as_interested")
+        self.mark_as_read = RenderExplorer("mark_as_read")
+        self.mark_as_liked = RenderExplorer("mark_as_liked")
+        self.mark_as_disliked = RenderExplorer("mark_as_disliked")
 
     @track_usage
     def load(self):
         try:
-            with open('abstract_stream.json', 'r') as f:
+            with open("abstract_stream.json", "r") as f:
                 py_version = json.load(f)
 
             self.rated_items = py_version.rated_items
@@ -96,26 +122,44 @@ class UserInterface(object):
     @track_usage
     def store(self):
         # Don't forget to store skipped items as unrated
-        raise NotImplementedError('store')
+        raise NotImplementedError("store")
 
     @track_usage
     def discover(self):
-        '''
+        """
         return something that renders
-        '''
-        raise NotImplementedError('discover')
+        """
+        raise NotImplementedError("discover")
 
     @track_usage
     def explore(self):
-        raise NotImplementedError('explore by RNG rating only')
+        if len(self.unrated_items) <= 2:
+            self._refill()
+
+        self.unrated_items.sort(
+            key=lambda record: max(
+                record["prng_score"], record["tfidf_score"], record["citation_score"]
+            )
+        )
+
+        self.active_item, self.unrated_items = (
+            self.unrated_items[0],
+            self.unrated_items[1:],
+        )
+
+        return RenderRecord(self.active_item)
+
+    def _refill(self):
+        raise NotImplementedError("download a batch of arXiv records")
 
     @track_usage
     def skip(self):
-        raise NotImplementedError('skip')
+        raise NotImplementedError("skip")
 
     @track_usage
     def download(self):
-        raise NotImplementedError('download')
+        raise NotImplementedError("download")
+
 
 ui = UserInterface()
 load = ui.load
@@ -129,6 +173,7 @@ l = liked = ui.mark_as_liked
 s = skip = ui.skip
 d = dislike = ui.mark_as_disliked
 download = ui.download
+
 
 def test(*, store=False):
     load()
@@ -156,5 +201,5 @@ def test(*, store=False):
         store()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     interact()
